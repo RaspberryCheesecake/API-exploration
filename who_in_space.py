e43c1cda2deaf0bb3ffd0cbcbf8c03562f308167
Hannah Hazi <hannah.hazi@cantab.net>
2016-12-18 11:18:40 +0000

    Turned it into functions, to better add some testing

--- who_in_space.py
+++ who_in_space.py
@@ -1,21 +1,29 @@
 import requests
 import json
 
-response = requests.get("http://api.open-notify.org/astros.json")
+def get_who_in_space():
+    response = requests.get("http://api.open-notify.org/astros.json")
+    
+    if response.status_code != 200:
+        print "I failed with error code %i " % response.status_code
 
-if response.status_code != 200:
-    print "I failed with error code %i " % response.status_code
+    data = response.json()
+    return data
 
-data = response.json()
+def display_who_in_space():
+    data = get_who_in_space()
+    print "The number of people currently in space is : %i " % data["number"]
+    
+    max_name = max(len(member["name"]) for member in data["people"])
+    max_craft = max(len(member["craft"]) for member in data["people"])
+    
+    print "They are :\n"
+    print "|".join(["Name".ljust(max_name),"Craft"])
+    print "-" * max_name + "|" + "-" * (max_craft + 2)
+    
+    for member in data["people"]:
+        print "|".join([member["name"].ljust(max_name), member["craft"]])
 
-print "The number of people currently in space is : %i " % data["number"]
 
-max_name = max(len(member["name"]) for member in data["people"])
-max_craft = max(len(member["craft"]) for member in data["people"])
-
-print "They are :\n"
-print "|".join(["Name".ljust(max_name),"Craft"])
-print "-" * max_name + "|" + "-" * (max_craft + 2)
-
-for member in data["people"]:
-    print "|".join([member["name"].ljust(max_name), member["craft"]])
+if __name__ == "__main__":
+    display_who_in_space()
