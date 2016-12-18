f5c6508080bfd421c3a3c289dc3f315ec6ffe316
Hannah Hazi <hannah.hazi@cantab.net>
2016-12-18 18:00:44 +0000

    Improved it a little - triple quotes are easier to work with for long strings, but really still need something more flexible entirely...

--- test_utils.py
+++ test_utils.py
@@ -5,24 +5,24 @@ class TestFulfillsChallenge47(unittest.TestCase):
 
     def setUp(self):
         self.mock_data = {"people" : [
-            {"craft": "ISS", "name": "Buzz Aldrin"},
-            {"craft": "ISS", "name": "Luke Skywalker"},
-            {"craft": "ISS", "name": "Jules Verne"}
+            {"craft": "Apollo", "name": "Buzz Aldrin"},
+            {"craft": "Millenium Falcon", "name": "Luke Skywalker"},
+            {"craft": "Millenium Falcon", "name": "Han Solo"}
             ]}
         self.mock_data["message"] = "success"
         self.mock_data["number"] = 3
 
     def test_width_of_column_wide_enough(self):
         result = display_who_in_space(self.mock_data)
-
-        expected_result = ("The number of people currently in space is : 3\n"
-                           "They are :\n"
-                           "Name          |Craft\n"
-                           "--------------|-----\n"
-                           "Buzz Aldrin   |ISS\n"
-                           "Luke Skywalker|ISS\n"
-                           "Jules Verne   |ISS\n" )
-
+ 
+        expected_result = """The number of people currently in space is : 3
+They are :
+Name          |Craft
+--------------|------------------
+Buzz Aldrin   |Apollo
+Luke Skywalker|Millenium Falcon
+Han Solo      |Millenium Falcon
+"""
         self.assertEqual(result, expected_result)
 
     def tearDown(self):
