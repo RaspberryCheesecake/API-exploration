c2f8feebd54f76263fb8795ccf6aa40be070e8a9
Hannah Hazi <hannah.hazi@cantab.net>
2016-12-18 17:43:32 +0000

    Adding mock data to test the conditions

--- test_utils.py
+++ test_utils.py
@@ -4,9 +4,16 @@ from who_in_space import get_who_in_space, display_who_in_space
 class TestFulfillsChallenge47(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.mock_data = {"people" : [
+            {"craft": "ISS", "name": "Buzz Aldrin"},
+            {"craft": "ISS", "name": "Luke Skywalker"},
+            {"craft": "ISS", "name": "Jules Verne"}
+            ]}
+        self.mock_data["message"] = "success"
+        self.mock_data["number"] = 3
 
     def test_width_of_column_wide_enough(self):
+        display_who_in_space(self.mock_data)
         self.assertEqual('foo'.upper(), 'FOO')
 
     def tearDown(self):
